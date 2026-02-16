582456271d8e7452e153b4cbfd8f0b5123383622
petrasvestartas <petrasvestartas@gmail.com>
2026-02-16 06:19:41 +0100

    Nurbsurface domain test in cpp

--- examples/rhino_scene_nurbssurface_json.py
+++ examples/rhino_scene_nurbssurface_json.py
@@ -7,7 +7,8 @@ reload_all()
 
 from session_rhino.session import Session
 
-filepath = r"c:\pc\3_code\code_rust\session\session_data\primitives.pb"
+# filepath = r"c:\pc\3_code\code_rust\session\session_data\primitives.pb"
+filepath = r"c:\pc\3_code\code_rust\session\session_data\surface.pb"
 data = Session.load(filepath)
 
 scene = Session()
