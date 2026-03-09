9463b1e48a30eac2915425900a02ec0b40d2d123
petrasvestartas <petrasvestartas@gmail.com>
2026-03-09 18:31:48 +0100

    mesh_loft python optimizaton

--- examples/rhino_scene_brep_curves_debug.py
+++ examples/rhino_scene_brep_curves_debug.py
@@ -13,7 +13,7 @@ from session_rhino.rhino_mesh import to_rhino
 import Rhino
 import System
 
-filepath = r"C:\pc\3_code\code_rust\session\session_data\mesh_quad_tri_loft8_python_out.pb" 
+filepath = r"C:\pc\3_code\code_rust\session\session_data\mesh_quad_tri_loft2_python_out.pb" 
 # filepath = r"c:\pc\3_code\code_rust\session\session_data\mesh_recheck.pb"
 
 data = PySession.pb_load(filepath)
@@ -22,3 +22,4 @@ meshes = list(data.objects.meshes)
 
 scene = Session.load(filepath)
 scene.draw(delete=True)
+
