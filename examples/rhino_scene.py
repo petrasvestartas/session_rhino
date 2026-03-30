b6fe63b387996ae1800f145826747559d7091b38
Petras Vestartas <petrasvestartas@gmail.com>
2026-03-30 20:58:53 +0200

    fix interval/discontinuity exact comparison for ARM FP

---
+++ examples/rhino_scene.py
@@ -0,0 +1,25 @@
+#! python3
+# venv: session_py
+
+
+from session_py.reload import reload_all
+import session_rhino
+reload_all()
+
+
+
+from session_rhino.session import Session
+from session_py.session import Session as PySession
+from session_rhino.rhino_mesh import to_rhino
+import Rhino
+import System
+
+filepath = r"C:\brg\code_rust\session\session_cpp\bool_intersect.pb"
+
+data = PySession.pb_load(filepath)
+meshes = list(data.objects.meshes)
+
+
+scene = Session.load(filepath)
+scene.draw(delete=True)
+
