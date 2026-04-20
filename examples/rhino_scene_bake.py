ae478deda00b532254a7f2d232bccfa3e5b77cce
Petras Vestartas <petrasvestartas@gmail.com>
2026-04-20 07:42:21 +0200

    wood

--- examples/rhino_scene_bake.py
+++ examples/rhino_scene_bake.py
@@ -29,13 +29,13 @@ SESSION_CONFIG.explode_mesh_faces = True
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_outofplane_dodecahedron.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_outofplane_icosahedron.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_outofplane_octahedron.pb"
-# filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_hexboxes.pb"
+filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_hexboxes.pb"
 
 # ── side-to-side in-plane ─────────────────────────────────────────────────
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_butterflies.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_hexshell.pb"  
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_differentdirections.pb"
-filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_hilti.pb" # no butterflies at a ll, there must be butterflies merged with polylines
+# filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_hilti.pb" # no butterflies at a ll, there must be butterflies merged with polylines
 
 # ── simple corners (mixed in/out-of-plane) ────────────────────────────────
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_simple_corners.pb"
@@ -56,7 +56,7 @@ filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_inplane_hilti.pb" # n
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_corners.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_corner.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_hexshell.pb"
-# filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_hexshell_reciprocal.pb"
+filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_hexshell_reciprocal.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_shell.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_vda_single_arch.pb"
 # filepath = r"C:\brg\code_rust\session\session_data\WoodF2F_cross_and_sides_corner.pb"
